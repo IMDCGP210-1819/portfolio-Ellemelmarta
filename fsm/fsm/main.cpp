90ad0f04a3b009a12a4d8b33072ad151264c00dd
Ben Carter <32357076+Ellemelmarta@users.noreply.github.com>
2018-10-05 12:36:00 +0100

    Switch statement fsm
    Basic switch fsm based on user input displays what the "ai" is doing to react to each of the players actions

--- fsm/fsm/main.cpp
+++ fsm/fsm/main.cpp
@@ -1,42 +1,62 @@
 #include <iostream>
-#include <random>
-#include <time.h>
-
+#include <string>
+#include <chrono>
+#include <thread>
 
 int main()
 {
-	int currentState;
+	using namespace std::this_thread; //allows for use of sleep for without std::
+	using namespace std::chrono; // allows use of seconds without std::
 
-	//sets rand to be something other than 1 almost like initalising it
-	srand(time(NULL));
+	char answer = '0';
 
-	currentState = (rand() % 5);
+	//infinitly loops the switch (only way to break out is by pulling your gun on the AI)
+loop:
+	while (true) {
+	
+		switch (answer)
+		{
 
-	int aiStates = currentState;
+		case '0':
+			std::cout << "\nThe AI is currently idle\n";
+			break;
 
-	//will output the case based on random number need to find a way to repeat
-	//this multiple times after a set amount of time and change the random number
-	//or base it on user input easily done by just changing the case.
-	switch(aiStates)
-	{
+		case '1':
+			std::cout << "\nThe AI is took your order\n";
+			sleep_for(seconds(1));
+			std::cout << "\nThe AI is currently cooking\n";
+			sleep_for(seconds(1));
+			std::cout << "\n The AI served your food\n";
+			break;
 
-	case 0:
-		std::cout << "Idle\n";
-		break;
-	case 1:
-		std::cout << "Walking\n";
-		break;
-	case 2:
-		std::cout << "Running\n";
-		break;
-	case 3:
-		std::cout << "Jumping\n";
-		break;
-	case 4:
-		std::cout << "Killing\n";
-		break;
+		case '2':
+			std::cout << "\nThe AI is currently cleaning\n";
+			break;
 
-	};
+		case '3':
+			std::cout << "\nThe AI has Hid for cover\n";
+			sleep_for(milliseconds(500));
+			std::cout << "\nThis AI ain't no rookie bot, he teleports behind you and kills you. GAME OVER\n";
+			sleep_for(seconds(2));
+			return 0;
+
+		case '4':
+			std::cout << "\nThe AI is staring at you confused\n";
+			break;
+			
+		default:
+			std::cout << "\nYou did not enter a valid option\n";
+			break;
+		};
+
+		std::cout << "\nPlease choose one of the following options:\n";
+		std::cout << "0 = Do nothing.\n";
+		std::cout << "1 = Sit down at a table\n";
+		std::cout << "2 = Make a mess\n";
+		std::cout << "3 = Pull out your gun.\n";
+		std::cout << "4 = Run into a wall and hurt yourself.\n";
+		std::cin >> answer;
+	}
 	
 	return 0;
 };
