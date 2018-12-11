80056a9e8ffe65d429f1a08783ada78c62e877aa
Ben Carter <32357076+Ellemelmarta@users.noreply.github.com>
2018-12-11 17:27:27 +0000

    Working Behaviour Tree
    +Main run function and creation of the behaviour tree
    
    +New example of exactly how the tree works at top
    
    +2 New tasks added for tree
    
    This behaviour tree shows a person going to cook a meal but first has to get the ingredients to be able to cook.
    This can be changed through the code at line 226 and changing the variables in the CookingStatus will affect the tree differently

--- BehaviourTree/BehaviourTrees/BehaviourTrees/main.cpp
+++ BehaviourTree/BehaviourTrees/BehaviourTrees/main.cpp
@@ -5,13 +5,15 @@
 using namespace std;
 
 /*
-									   start
-									   	 |
-							selector (have ingredients?)
-					/									\
-				sequence							   sequence
-			/		|								/			  \
-		walk to		pick up	  				walk to cooker	     cook
+								Root
+								  |
+							  Sequence 1
+						/					 \
+			Selector1						Sequence2
+			/		\					 /				\
+Got Ingredients?	Sequence3		WalkToCooker	  CookFood
+				  /			\
+		   	 WalkToIng	 PickUpIng
 
 */
 
@@ -118,11 +120,13 @@ public:
 			status->distanceToIngredients--;
 
 			if (status->distanceToIngredients > 1)
-				cout << "The sim is now" << status->distanceToIngredients << " meters from the ingredients." << endl;
-
+			{
+				cout << "The sim is now " << status->distanceToIngredients << " meters from the ingredients." << endl;
+			}
 			else if (status->distanceToIngredients == 1)
+			{
 				cout << "The sim is now only 1 meter away from the ingredients." << endl;
-
+			}
 			else
 				cout << "The person is at the ingredients." << endl;
 		}
@@ -156,18 +160,137 @@ public:
 
 class WalkToCookerTask : public Node
 {
+private:
+	CookingStatus* status;
 
+public:
+	WalkToCookerTask(CookingStatus* status) : status(status) {}
+	virtual bool willRun() override
+	{
+		if (status->distanceToCooker > 0)
+		{
+			cout << "The sim approaches the cooker." << endl;
+			status->distanceToCooker--;
+
+			if (status->distanceToCooker > 1)
+			{
+				cout << "The sim is " << status->distanceToCooker << " from the cooker." << endl;
+			}
+			else if (status->distanceToCooker == 1)
+			{
+				cout << "The sim is now only 1 metre from the cooker." << endl;
+			}
+			else
+			{
+				cout << "The sime is now at the cooker" << endl;
+			}
+		}
+		return true;
+	}
 };
 
 
 class PutOnCookerTask : public Node
 {
-
+private:
+	CookingStatus* status;
+	
+public:
+	PutOnCookerTask(CookingStatus* status) : status(status) {}
+	virtual bool willRun() override
+	{
+		if (status->distanceToCooker > 0)
+		{
+			cout << "The sim is too far from the cooker to cook the ingredients." << endl;
+			return false;
+		}
+		else
+		{
+			status->isCooked = true;
+			cout << "The sim cooks the food" << endl;
+			return true;
+		}
+	}
 };
 
 
 int main()
 {
+	Selector *root = new Selector, *selector1 = new Selector;
 
-	return 0;
+	Sequence *sequence1 = new Sequence;
+	Sequence *sequence2 = new Sequence;
+	Sequence *sequence3 = new Sequence;
+
+	//sets it so sim hasnt cooked and has no ingredients and is both 3 meters from ingredients and cooker
+	CookingStatus* cookingStatus = new CookingStatus{ false, false, 3, 3 };
+
+	CheckIfIngredientsNeededTask* checkIngredients = new CheckIfIngredientsNeededTask(cookingStatus);
+	WalkToIngredientsTask* walkIngredients = new WalkToIngredientsTask(cookingStatus);
+	PickUpTheIngredientsTask* pickUpIngredients = new PickUpTheIngredientsTask(cookingStatus);
+	WalkToCookerTask* walkCooker = new WalkToCookerTask(cookingStatus);
+	PutOnCookerTask* cookFood = new PutOnCookerTask(cookingStatus);
+
+	//root is sequence as both need to return true before the task is finished (food is cooked)
+	root->addChild(sequence1);
+
+	//ingredients is selector as if they already have ingredients it can return true and be a completed branch
+	sequence1->addChild(selector1);
+
+	//cooking has to be sequence as it means walking to cooker and cooking the food
+	//(could be changed to selector for the "isCooked") 
+	sequence1->addChild(sequence2);
+
+	//check or get ingredients branch
+	selector1->addChild(checkIngredients);
+	selector1->addChild(sequence3);
+
+	sequence3->addChild(walkIngredients);
+	sequence3->addChild(pickUpIngredients);
+
+	//cooking branch
+	sequence2->addChild(walkCooker);
+	sequence2->addChild(cookFood);
+
+	while (!root->willRun())
+	{
+		cout << "---------------------------------" << endl;
+	}
+	cout << endl << "Operation Complete. Behaviour tree exited" << endl;
+	cin.get();
+
+
+	/*
+				 OUTPUT
+	The sim has not got the ingredients.
+	The sim approaches the ingredients.
+	The sim is now 2 meters from the ingredients.
+	The sim is too far from the ingredients to pick them up.
+	---------------------------------
+	The sim has not got the ingredients.
+	The sim approaches the ingredients.
+	The sim is now only 1 meter away from the ingredients.
+	The sim is too far from the ingredients to pick them up.
+	---------------------------------
+	The sim has not got the ingredients.
+	The sim approaches the ingredients.
+	The person is at the ingredients.
+	The sim picked up the ingredients.
+	The sim approaches the cooker.
+	The sim is 2 from the cooker.
+	The sim is too far from the cooker to cook the ingredients.
+	---------------------------------
+	The sim has the ingredients needed.
+	The sim approaches the cooker.
+	The sim is now only 1 metre from the cooker.
+	The sim is too far from the cooker to cook the ingredients.
+	---------------------------------
+	The sim has the ingredients needed.
+	The sim approaches the cooker.
+	The sime is now at the cooker
+	The sim cooks the food
+
+	Operation Complete. Behaviour tree exited
+
+	*/
 }
